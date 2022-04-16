4f79878006a1f24770393fa05485ff25943b119e
Gulnaz Myrzakulova <gulnaz.myrzakulova@gmail.com>
2022-04-16 18:57:53 +0600

    Implemet

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
-console.log(data)
+const { useState } = React
 
 function App() {
-  const [product, setProduct] = useState()
-  const buttons = data.map((product) =>(
-    <button className="product" key = {product.id} >
-      <div>{product.title}</div>
-      <div>{product.price} c</div>
-    </button>
-  ))
+  const [basket, setBasket] = useState([]);
+
+  const addToBasket = (item) => {
+    const newBasket = [...basket, item]
+    setBasket(newBasket)
+  }
+
+  const removeFromBasket = (product) => {
+    const newBasket = basket.filter((item) => item.id !== product.id)
+    setBasket(newBasket)
+  }
+
+  const getBasketTotal = () => {
+    let sum = 0
+    basket.forEach((item) => {
+      sum += item.price
+    })
+    return sum
+  }
 
   return (
     <div className="App">
-      <div className="product-list">{buttons} </div>
+      <div className="product-list">
+        {data.map((product) => (
+          <button className="product" key={product.id} onClick={() => addToBasket(product)}>
+            <div>{product.title}</div>
+            <div>{product.price} c</div>
+          </button>
+        ))}
+      </div>
       <div className="basket">
         <div className="total">
           <span>Итого:</span>
-          <span>0 c</span>
+          <span>{getBasketTotal()} c</span>
         </div>
         <div>
-          <button className="product" >
-            <div>Название</div>
-            <div>Цена c</div>
-          </button>
+          {basket.map((product, i) => (
+            <button
+              className="product"
+              key={i}
+              onClick={() => removeFromBasket(product)}
+            >
+              <div>{product.title}</div>
+              <div>{product.price} c</div>
+            </button>
+          ))}
         </div>
       </div>
     </div>
   );
 }
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 ReactDOM.render(<App />, root);
